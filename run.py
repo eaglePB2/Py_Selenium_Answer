2767222417e3a9b0363563283910f544fd7787f3
EaglePB2 <eagleforceleader@hotmail.com>
2022-12-28 12:49:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,25 +10,30 @@ import random
 
 driver = webdriver.Edge()
 
-#open tab
+# Input
+ClassName = input("班級: ")
+Name = input("姓名: ")
+SeatNo = input("座號: ")
+ChooseNo = input("挑選: < 10 | 20 | 25 | 40 | 50 | 80 | 100 | 125 | 150 | 200 | 250>")
+ExamTime = input("測驗時間: < 1 | 3 | 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40>")
+
+# open tab
 # ... take the code from the options below
-driver.get("http://etabc.tw/etest/default.asp?dt=1&db=3012")
+driver.get("http://etabc.tw/etest/default.asp?dt=1&db=3012&ct=%BC%EA%B4%F2&sname=%BC%EA%B4%F2%AE%FC%A8%C6")
 
 # Setting up the tests...
 driver.find_element(By.NAME, "endno").send_keys("\ue003\ue003\ue003\ue0033012")
-driver.find_element(By.NAME, "classname").send_keys("122")
-driver.find_element(By.NAME, "name").send_keys("测试用")
-driver.find_element(By.NAME, "seatno").send_keys("36")
-Select(driver.find_element("name",'chooseno')).select_by_visible_text('100')
-Select(driver.find_element("name",'examtime')).select_by_value('15')
+driver.find_element(By.NAME, "classname").send_keys(ClassName)
+driver.find_element(By.NAME, "name").send_keys(Name)
+driver.find_element(By.NAME, "seatno").send_keys(SeatNo)
+Select(driver.find_element("name",'chooseno')).select_by_visible_text(ChooseNo)
+Select(driver.find_element("name",'examtime')).select_by_value(ExamTime)
 
 driver.find_element(By.XPATH, value="//input[@value='開始測驗']").click()
 
-# Make the tests...
+# Attempting the tests...
 
-
-
-#dictionary attack
+# Dictionary attack
 convert = [i.strip().split(' = ') for i in open('dictionary.txt', 'r', encoding='UTF-8')]
 for i in range(100):
     value = driver.find_element(By.XPATH, value = "/html/body/form/div/table/tbody/tr[" + str(i+3) + "]/td[2]").text
